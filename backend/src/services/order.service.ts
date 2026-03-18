e7c4c7924312a7fbf939816d5914787ede1f7515
hrithik <hrithikmails88@gmail.com>
2026-03-18 23:22:48 +0530

    added transaction during checkout

--- backend/src/services/order.service.ts
+++ backend/src/services/order.service.ts
@@ -2,51 +2,56 @@ import { prisma } from "../config/prisma";
 
 export class OrderService {
 
-  static async checkout(buyerId: string, paymentMethod: "ONLINE" | "COD") {
+  static async checkout(
+    buyerId: string,
+    paymentMethod: "ONLINE" | "COD"
+  ) {
 
-    const cart = await prisma.cart.findUnique({
-      where: { buyerId },
-      include: {
-        items: {
-          include: {
-            product: true
+    return await prisma.$transaction(async (tx) => {
+
+      const cart = await tx.cart.findUnique({
+        where: { buyerId },
+        include: {
+          items: {
+            include: {
+              product: true
+            }
           }
-        }
-      }
-    });
-
-    if (!cart || cart.items.length === 0) {
-      throw new Error("Cart is empty");
-    }
-
-    const shopId = cart.items[0].product.shopId;
-
-    const order = await prisma.order.create({
-      data: {
-        buyerId,
-        shopId,
-        paymentMethod
-      }
-    });
-
-    for (const item of cart.items) {
-
-      await prisma.orderItem.create({
-        data: {
-          orderId: order.id,
-          productId: item.productId,
-          quantity: item.quantity,
-          price: item.price
         }
       });
 
-    }
+      if (!cart || cart.items.length === 0) {
+        throw new Error("Cart is empty");
+      }
 
-    await prisma.cartItem.deleteMany({
-      where: { cartId: cart.id }
+      const shopId = cart.items[0].product.shopId;
+
+      const order = await tx.order.create({
+        data: {
+          buyerId,
+          shopId,
+          paymentMethod
+        }
+      });
+
+      for (const item of cart.items) {
+        await tx.orderItem.create({
+          data: {
+            orderId: order.id,
+            productId: item.productId,
+            quantity: item.quantity,
+            price: item.price
+          }
+        });
+      }
+
+      await tx.cartItem.deleteMany({
+        where: { cartId: cart.id }
+      });
+
+      return order;
+
     });
-
-    return order;
 
   }
 
